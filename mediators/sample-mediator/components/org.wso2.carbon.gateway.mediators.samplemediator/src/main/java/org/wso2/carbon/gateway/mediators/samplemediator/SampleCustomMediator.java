29d4fa2259085d6148a404d378c411e9177aab74
Pubudu Fernando <pubudu.12@cse.mrt.ac.lk>
2016-04-29 15:05:54 +0530

    Sample mediator modified to process an XML payload

--- mediators/sample-mediator/components/org.wso2.carbon.gateway.mediators.samplemediator/src/main/java/org/wso2/carbon/gateway/mediators/samplemediator/SampleCustomMediator.java
+++ mediators/sample-mediator/components/org.wso2.carbon.gateway.mediators.samplemediator/src/main/java/org/wso2/carbon/gateway/mediators/samplemediator/SampleCustomMediator.java
@@ -17,17 +17,34 @@
  */
 package org.wso2.carbon.gateway.mediators.samplemediator;
 
+import net.sf.saxon.Configuration;
+import net.sf.saxon.lib.NamespaceConstant;
+import net.sf.saxon.om.DocumentInfo;
+import net.sf.saxon.xpath.XPathFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.gateway.core.config.ParameterHolder;
 import org.wso2.carbon.gateway.core.flow.AbstractMediator;
+import org.wso2.carbon.gateway.core.flow.contentaware.ByteBufferBackedInputStream;
 import org.wso2.carbon.messaging.CarbonCallback;
 import org.wso2.carbon.messaging.CarbonMessage;
+import org.xml.sax.InputSource;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.namespace.QName;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.xpath.*;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Sample Custom Mediator
  */
-public class SampleCustomMediator extends AbstractMediator {
+public class SampleCustomMediator extends AbstractMediator implements XPathVariableResolver, NamespaceContext {
 
     private static final Logger log = LoggerFactory.getLogger(SampleCustomMediator.class);
     private String logMessage = "Message received at Custom Sample Mediator";
@@ -46,9 +63,42 @@ public class SampleCustomMediator extends AbstractMediator {
 
     @Override
     public boolean receive(CarbonMessage carbonMessage, CarbonCallback carbonCallback) throws Exception {
-        log.info(logMessage);
+        BlockingQueue<ByteBuffer> content = new LinkedBlockingQueue<>(carbonMessage.getFullMessageBody());
+        InputStream stream = new ByteBufferBackedInputStream(content);
+        go(stream, "//LINE");
         return next(carbonMessage, carbonCallback);
+    }
 
+    public void go(InputStream stream, String query) throws Exception {
+
+        // Following is specific to Saxon: should be in a properties file
+        System.setProperty("javax.xml.xpath.XPathFactory:" + NamespaceConstant.OBJECT_MODEL_SAXON,
+                "net.sf.saxon.xpath.XPathFactoryImpl");
+
+        XPathFactory xpf = XPathFactory.newInstance(NamespaceConstant.OBJECT_MODEL_SAXON);
+        XPath xpe = xpf.newXPath();
+        System.err.println("Loaded XPath Provider " + xpe.getClass().getName());
+
+        long startTime = System.currentTimeMillis();
+        // Build the source document. This is outside the scope of the XPath API, and
+        // is therefore Saxon-specific.
+        InputSource is = new InputSource(stream);
+        SAXSource ss = new SAXSource(is);
+        Configuration config = ((XPathFactoryImpl) xpf).getConfiguration();
+        DocumentInfo doc = config.buildDocument(ss);
+
+        // Declare a variable resolver to return the value of variables used in XPath expressions
+        xpe.setXPathVariableResolver(this);
+
+        XPathExpression findLine = xpe.compile(query);
+
+        List matchedLines = (List) findLine.evaluate(doc, XPathConstants.NODESET);
+
+        long endTime = System.currentTimeMillis();
+
+//        Utils.printResult(endTime - startTime, matchedLines.size() + "");
+        log.info("Time elapsed: " + (endTime - startTime));
+        log.info("Elements found: " + matchedLines.size());
     }
 
     public String getLogMessage() {
@@ -57,5 +107,27 @@ public class SampleCustomMediator extends AbstractMediator {
 
     public void setLogMessage(String logMessage) {
         this.logMessage = logMessage;
+    }
+
+    @Override
+    public Object resolveVariable(QName qName) {
+        return null;
+    }
+
+    public String getNamespaceURI(String prefix) {
+        System.err.println("Looking up: " + prefix);
+        if (prefix.equals("saxon")) {
+            return "http://saxon.sf.net/";
+        } else {
+            return null;
+        }
+    }
+
+    @Override public String getPrefix(String namespaceURI) {
+        return null;
+    }
+
+    @Override public Iterator getPrefixes(String namespaceURI) {
+        return null;
     }
 }
